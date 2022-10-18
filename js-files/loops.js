8acbd965fc3945ed152b41387fac4c2af250c729
KeepKindling <flashywizard127@gmail.com>
2022-10-18 09:27:07 +0000

    add loops.js file

--- js-files/loops.js
+++ js-files/loops.js
@@ -4,9 +4,23 @@ and the loop will continue while this statement is true or equal to true. The th
 and final statement is called the incrementExpression. Typically we use the increment
 operator 'i+' and then decide by how much we want to increment i by.
 Then inside the brackets we can create 1 or more statements which will be
-executed while the for loops condition is true.
-*/
+executed while the for loops condition is true. */
 
-for (let i = 0; i < 5; i++) {
-    console.log("Hello World!")
+// for (let i = 0; i < 5; i++) {
+//     console.log("Hello World!")
+// }
+
+
+/* A while loop requires a condition inside of the parenthesis. While this condition
+is true, the code inside the loop will continue to run. Inside the condition, we 
+are saying while the value of loading "0" is less than 100, print this message 
+and then add 1 to loading. */
+
+
+let loading = 0
+
+while(loading < 100) {
+    console.log("Website is still loading");
+
+    loading++;
 }
